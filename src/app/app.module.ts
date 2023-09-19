ec230c2e5907ab9e5e95e1a31abfea7df35880bb
RedaDamene <damene.reda4@gmail.com>
2023-09-19 11:13:25 +0200

    Feat: Init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { CarbonFootprintComponent } from './component/carbon-footprint/carbon-footprint.component';
+import { CarbonFootprintFormComponent } from './component/carbon-footprint-form/carbon-footprint-form.component';
+import { CarbonFootprintResultComponent } from './component/carbon-footprint-result/carbon-footprint-result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CarbonFootprintComponent,
+    CarbonFootprintFormComponent,
+    CarbonFootprintResultComponent
   ],
   imports: [
     BrowserModule,
